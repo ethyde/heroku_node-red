09dccc30361923d26ee2b7888425e863548a8fce
Emmanuel Plouvier <eplouvie@prismamedia.com>
2017-05-25 23:08:36 +0200

    oups

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import { createServer } from 'http'
 
 import app from './app/app'
 
-dbConnect(() => {
-  console.log('✔ Connection established to mongoDB database')
+const server = createServer(app)
 
-  const server = createServer(app)
-
-  server.listen(app.get('port'), () => {
-    console.log('✔ Server listening on port', app.get('port'))
-  })
-
+server.listen(app.get('port'), () => {
+  console.log('✔ Server listening on port', app.get('port'))
 })
+// dbConnect(() => {
+//   console.log('✔ Connection established to mongoDB database')
+
+
+// })
