f69515c41405c0ddb7da5352a5672410bb3c2c72
Emmanuel Plouvier <eplouvie@prismamedia.com>
2017-05-26 16:54:40 +0200

    after reset

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
 import { createServer } from 'http'
-// load node-red
-import RED from 'node-red'
-// and his settings
-import settings from './app/node-red.settings'
 
-// Load App
 import app from './app/app'
 
 const server = createServer(app)
 
-// Setup node-red ==============================================================
-RED.init( server, settings )
-
-// Serve the editor UI from /red
-app.use( settings.httpAdminRoot, RED.httpAdmin )
-
-// Serve the http nodes UI from /api
-app.use( settings.httpNodeRoot, RED.httpNode )
-
 server.listen(app.get('port'), () => {
   console.log('✔ Server listening on port', app.get('port'))
-  // Start the runtime
-  RED.start()
 })
-
-// heroku logs --app glacial-island-98070 --tail
