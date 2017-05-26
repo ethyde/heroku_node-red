44a9b4f6d12393d110f855b7841c43b1e2628792
Emmanuel Plouvier <eplouvie@prismamedia.com>
2017-05-26 16:34:41 +0200

    fix for the not realy embed node-red

--- app/app.js
+++ app/app.js
@@ -3,34 +3,13 @@
 // set up ======================================================================
 // get all the tools we need
 import express  from 'express'
-import { createServer } from 'http'
 
 // configuration ===============================================================
-
-// load node-red
-import RED from 'node-red'
-// and his settings
-import settings from './node-red.settings'
-
 const app = express()
-
 
 // set up our express application
 app.set('port', process.env.PORT || 3000)
 app.use('/',express.static('public')) // server static content from public
-
-const server = createServer(app)
-// Setup node-red ==============================================================
-RED.init( server, settings )
-
-// Serve the editor UI from /red
-app.use( settings.httpAdminRoot, RED.httpAdmin )
-
-// Serve the http nodes UI from /api
-app.use( settings.httpNodeRoot, RED.httpNode )
-
-// Start the runtime
-RED.start()
 
 // Export to server.js =========================================================
 export default app
--- server.js
+++ server.js
@@ -1,5 +1,27 @@
+import { createServer } from 'http'
+// load node-red
+import RED from 'node-red'
+// and his settings
+import settings from './app/node-red.settings'
+
+// Load App
 import app from './app/app'
 
-app.listen(app.get('port'), () => {
+const server = createServer(app)
+
+// Setup node-red ==============================================================
+RED.init( server, settings )
+
+// Serve the editor UI from /red
+app.use( settings.httpAdminRoot, RED.httpAdmin )
+
+// Serve the http nodes UI from /api
+app.use( settings.httpNodeRoot, RED.httpNode )
+
+server.listen(app.get('port'), () => {
   console.log('✔ Server listening on port', app.get('port'))
+  // Start the runtime
+  RED.start()
 })
+
+// heroku logs --app glacial-island-98070 --tail
